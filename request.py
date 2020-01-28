9b59d2801b0fe84f41c15a77003eef42f6cac137
George Wang <georgewang89@gmail.com>
2020-01-27 22:04:36 -0500

    added markdown

--- request.py
+++ request.py
@@ -15,7 +15,13 @@ BUCKET_NAME = 'gwherokubucket'
 
 FOLDER = './images'
 FILENAME = 'upload.jpg'
-uploaded_file = st.file_uploader("Choose a jpg file", type=["jpg", "jpeg", "png"])
+st.markdown('# Opaki vs Zebra')
+st.markdown('**Step 1:** Download an image of an [opaki](https://www.google.com/search?q=opaki&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjM9oPtoaXnAhUiw1kKHf7ODgkQ_AUoAXoECA0QAw&cshid=1580179182106307&biw=1920&bih=946) or [zebra](https://www.google.com/search?q=zebra&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjT3rWFoqXnAhWQm1kKHcEQCq8Q_AUoAXoECBAQAw&biw=1920&bih=946)')
+st.markdown('**Step 2:** Upload your image below')
+st.markdown('**Step 3:** Click _Classify_ to see what my neural network thinks')
+
+
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     data = Image.open(uploaded_file)
     data.save(FOLDER+'/'+FILENAME)
@@ -43,4 +49,4 @@ if st.button("classify"):
     response = requests.post("{}/".format("http://127.0.0.1:5000"), json =post_data)
     response_loaded = json.loads(response.content)
     response_df = json_normalize(response_loaded)
-    st.write("Your image contains a "+ response_df["class"])
+    st.markdown("Your image contains a **"+response_df.iloc[0,0]+'**')
